f9f21e2ffd83ef832ed6f4a5c160ddf0ebb31bb4
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-05-01 20:28:24 -0700

    thank you modal styling completed

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <Main />
+      {/* <Main /> */}
       <ThankYou />
     </div>
   );
--- src/Components/ThankYou/ThankYou.jsx
+++ src/Components/ThankYou/ThankYou.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./ThankYou.css";
 
 const ThankYou = () => {
-  return <div></div>;
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <div className="thanks__content">
+      <img
+        src="../../illustration-thank-you.svg"
+        alt="thank you"
+        className="thanks__image"
+      />
+      <p className="thanks__selection">You have selected 4 out of 5</p>
+      <h1 className="thanks__title">Thank you!</h1>
+      <p className="thanks__text">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
+    </div>
+  );
 };
 
 export default ThankYou;
